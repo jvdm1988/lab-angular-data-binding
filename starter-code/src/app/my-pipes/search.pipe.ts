a39f1eb124f06a664ecbd489df9fd5e6b2398fe0
Jessicavandermarel <vandermareljessica@gmail.com>
2017-07-13 17:37:48 -0400

    added search function

--- starter-code/src/app/my-pipes/search.pipe.ts
+++ starter-code/src/app/my-pipes/search.pipe.ts
@@ -5,8 +5,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
+  transform(value: any, searchTerm: string): any {
+    if (!value) {
+      return[];
+    }
+    if (!searchTerm) {
+      return value;
+    }
 
+    let matchedResults = [];
+
+    searchTerm = searchTerm.toLowerCase();
+
+    value.forEach((oneItem) => {
+    // lower case the name so that it always match with searchTerm
+    // if the name has the searchterm in it, push it to the new array
+    if (oneItem.name.toLowerCase().includes(searchTerm)) {
+      matchedResults.push(oneItem);
+    }
+  });
+  return matchedResults
+  }
 }
