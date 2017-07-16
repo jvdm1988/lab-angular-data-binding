7180fddd9abec7350ff57e95af362fd6cfe05442
Jessicavandermarel <vandermareljessica@gmail.com>
2017-07-15 20:21:22 -0400

    iteration 4

--- starter-code/src/app/pipes/pipes.component.ts
+++ starter-code/src/app/pipes/pipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import foods from '../foods';
 
 @Component({
   selector: 'app-pipes',
@@ -7,11 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PipesComponent implements OnInit {
   foodSearchText: string = "";
+  showForm: boolean = false;
+  //            from foods.ts
+  //               |
+  foods: any[] = foods;
+  // |
+  // for view
+  todayArray: any = [];
+  totalCal: number = 0;
 
-  
   constructor() { }
 
   ngOnInit() {
   }
 
+  showFormFunction() {
+    this.showForm = !this.showForm;
+    console.log(this.showForm);
+  }
+
+  todaysFoodsFunction(oneFood) {
+    this.todayArray.push(oneFood);
+      return this.todayArray;
+    }
 }
